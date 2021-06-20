5c60b9ba1c611ad01e3da6854fd9f2507cd50feb
YongchieM <62608274+YongchieM@users.noreply.github.com>
2021-06-21 00:28:47 +0200

    Add files via upload

---
+++ include/LfD/viewer.hpp
@@ -0,0 +1,42 @@
+#ifndef __LFD_VIEWER_HPP__
+#define __LFD_VIEWER_HPP__
+
+#include <iostream>
+#include <string>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/common/common.h>
+#include <pcl/common/transforms.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+#include <pangolin/pangolin.h>
+
+
+void gen_point_cloud(cv::Mat& color, cv::Mat& depth, double factor, Eigen::Matrix3d K, pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud);
+
+
+void show_ellipsoid(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud, Eigen::MatrixXd& ellipsoids, Eigen::MatrixXd& campose);
+
+void rtToOpenGTMatrix(Eigen::Matrix4d& T, pangolin::OpenGlMatrix& M);
+
+
+Eigen::VectorXd evaluate_estimate(cv::Mat& depth, Eigen::VectorXd& bbs, double factor, Eigen::Matrix3d K, Eigen::MatrixXd& ellipsoids, Eigen::MatrixXd& campose);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
