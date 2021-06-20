554c28679f9ec223a76fc9a39b94ac43985abe99
YongchieM <62608274+YongchieM@users.noreply.github.com>
2021-06-21 00:50:36 +0200

    Update viewer.hpp

--- include/LfD/viewer.hpp
+++ include/LfD/viewer.hpp
@@ -15,26 +15,17 @@
 
 #include <pangolin/pangolin.h>
 
-
+//generate point cloud with depth image
 void gen_point_cloud(cv::Mat& color, cv::Mat& depth, double factor, Eigen::Matrix3d K, pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud);
 
-
+//draw ellipsoids
 void show_ellipsoid(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud, Eigen::MatrixXd& ellipsoids, Eigen::MatrixXd& campose);
 
+//convert Eigen::Matrix to OpenGlMatrix
 void rtToOpenGTMatrix(Eigen::Matrix4d& T, pangolin::OpenGlMatrix& M);
 
 
 Eigen::VectorXd evaluate_estimate(cv::Mat& depth, Eigen::VectorXd& bbs, double factor, Eigen::Matrix3d K, Eigen::MatrixXd& ellipsoids, Eigen::MatrixXd& campose);
-
-
-
-
-
-
-
-
-
-
 
 
 
