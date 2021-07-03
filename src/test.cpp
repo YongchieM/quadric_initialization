51a7d56cd28251590f5da7bebe038aa70e01e4a9
YongchieM <62608274+YongchieM@users.noreply.github.com>
2021-07-03 11:49:15 +0200

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -15,27 +15,29 @@ int main(int argc, char** argv)
     Eigen::MatrixXd pose,bbox;
     Eigen::Matrix3d K;
     
-    
     LfD::load_data("tum/cabinet/bbox.txt",bbox);
     LfD::load_data("tum/cabinet/campose.txt",pose);
     LfD::load_intrinsics("tum/cabinet/intrinsics.txt",K);
     
     Eigen::Matrix4d Q;
     
+    //select data for initialization
     Eigen::MatrixXd selected_bbox = bbox.topRows(9);
     Eigen::MatrixXd selected_pose = pose.topRows(9);
     
+    ///choose the method for initialization
     Q = LfD::generate_ellipsoid_from_ellipses(selected_pose,selected_bbox,K);
     //Q = LfD::generate_ellipsoid_from_planes(selected_pose,selected_bbox,K);
     
     cout<<"Q matrix is: "<<endl<<Q<<endl;
     
+    //an ellipsoid should have 3 positive eigen values
     Eigen::EigenSolver<Eigen::Matrix4d> es(Q);
     Eigen::Matrix4d D = es.pseudoEigenvalueMatrix();
     Eigen::Matrix4d V = es.pseudoEigenvectors();
     cout<<"eigen vlues are: "<<endl<<D<<endl;
     
-    // visualisation
+    // visualisation, choose associated rgb, depth and pose
     double factor = 5000.0;
     cv::Mat color,depth;
     color = cv::imread("tum/cabinet/rgb/1341841281.5546.jpg");
@@ -44,9 +46,7 @@ int main(int argc, char** argv)
     
     gen_point_cloud(color,depth,factor,K,cloud);
     
-    
     Eigen::MatrixXd pose_vis = pose.row(4);
-    
     show_ellipsoid(cloud,Q,pose_vis);
     
     return 0;
